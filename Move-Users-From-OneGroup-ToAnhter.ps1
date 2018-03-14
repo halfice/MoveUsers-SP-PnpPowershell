443be9d00410b1444d4f5dd874e862d287d4d80c
halfice <axix.szabist@gmail.com>
2018-03-14 13:59:04 +0400

    Move User to Group
    Move User to Group

---
+++ Move-Users-From-OneGroup-ToAnhter.ps1
@@ -0,0 +1,8 @@
+﻿Connect-PNPOnline -url "*******************" -Credentials MyManager 
+$Sourcegroup = Get-PnPGroup -Identity "GroupName"  | Select-Object Title,Users  
+$DestGroupTitle="Destination Group";
+foreach ($item in $Sourcegroup.Users)
+{
+$temp="i:0#.f|membership|"+$item.Email;
+Add-PnPUserToGroup  -LoginName $temp -Identity $GroupTitle
+}
